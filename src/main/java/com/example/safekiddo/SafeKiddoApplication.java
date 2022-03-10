340113e9c9ffa4586364049b7d78fa7d31c9596c
Radoslaw Lazur <radoslaw.lazur@azimo.com>
2022-03-10 10:45:50 +0100

    test

--- src/main/java/com/example/safekiddo/SafeKiddoApplication.java
+++ src/main/java/com/example/safekiddo/SafeKiddoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SafeKiddoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SafeKiddoApplication.class, args);
     }
-
 }
